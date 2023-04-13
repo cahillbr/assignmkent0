2fec032911ab0b528fd296e3eadb5fd33e4947ca
cahillbr <cahillbr@oregonstate.edu>
2023-04-12 22:05:46 -0700

    w

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Brendan Cahill
+# OSU Email: cahillbr@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 4/ 12 / 23
+# Description: Testing assignment to determine python version
+
+
+def gradescope_test():
+    return 42
